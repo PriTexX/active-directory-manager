b96e2c0f062deadd9523269090ed4db43b76a345
pritexx <vladon_2004@mail.ru>
2022-12-29 09:38:50 +0300

    added DomainItemFactory

---
+++ ActiveDirectoryManager/ItemFactory/DomainItemFactory.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.DirectoryServices;
+using System.Runtime.Versioning;
+using ActiveDirectoryManager.ActiveDirectoryItem;
+
+namespace ActiveDirectoryManager.ItemFactory;
+
+[SupportedOSPlatform("windows")]
+public class DomainItemFactory : IDomainItemFactory
+{
+    public DomainItem CreateInstance(SearchResult searchResult, DomainItemType type)
+    {
+        DomainItem domainItem = type switch
+        {
+            DomainItemType.User => new UserItem(),
+            DomainItemType.Group => new GroupItem(),
+            DomainItemType.Container => new ContainerItem(),
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+
+        domainItem.SearchResult = searchResult;
+        domainItem.DomainItemType = type;
+        
+        foreach (string ldapField in searchResult.Properties.PropertyNames)
+        {
+            var values = (from object? value in searchResult.Properties[ldapField] select value.ToString()).ToList();
+            domainItem.PropertyCollection.Add(ldapField, values);
+        }
+
+        return domainItem;
+    }
+    
+    public DomainItem CreateInstance(DirectoryEntry dirEntry, DomainItemType type, string[] propertiesToLoad)
+    {
+        DomainItem domainItem = type switch
+        {
+            DomainItemType.User => new UserItem(),
+            DomainItemType.Group => new GroupItem(),
+            DomainItemType.Container => new ContainerItem(),
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+
+        domainItem.DirectoryEntry = dirEntry;
+        domainItem.DomainItemType = type;
+        
+        domainItem.DirectoryEntry.RefreshCache(propertiesToLoad.Concat(new []{"distinguishedname"}).ToArray());
+        
+        foreach (var propertyName in propertiesToLoad)
+        {
+            PropertyValueCollection valueCollection = dirEntry.Properties[propertyName];
+            domainItem.PropertyCollection.Add(propertyName, new List<string?>());
+            IEnumerator en = valueCollection.GetEnumerator();
+
+            while (en.MoveNext())
+            {
+                if (en.Current != null)
+                {
+                    domainItem.PropertyCollection[propertyName].Add(en.Current.ToString());
+                }
+            }
+        }
+
+        domainItem.DistinguishedName = domainItem.DirectoryEntry.Properties["distinguishedname"].Value.ToString();
+
+        return domainItem;
+    }
+}
---
+++ ActiveDirectoryManager/ItemFactory/IDomainItemFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.DirectoryServices;
+using ActiveDirectoryManager.ActiveDirectoryItem;
+
+namespace ActiveDirectoryManager.ItemFactory;
+
+public interface IDomainItemFactory
+{
+    public DomainItem CreateInstance(DirectoryEntry dirEntry, DomainItemType type, string[] propertiesToLoad);
+    public DomainItem CreateInstance(SearchResult searchResult, DomainItemType type);
+}
