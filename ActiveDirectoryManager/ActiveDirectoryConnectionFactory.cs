27a6dae8a9c54d2e83e841c0c164ac62117c27db
pritexx <vladon_2004@mail.ru>
2023-01-04 15:16:45 +0300

    created empty classes for future

---
+++ ActiveDirectoryManager/ActiveDirectoryConnectionFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.DirectoryServices.AccountManagement;
+
+namespace ActiveDirectoryManager;
+
+public class ActiveDirectoryConnectionFactory
+{
+    public PrincipalContext GetConnection()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ActiveDirectoryManager/ActiveDirectoryManager.cs
@@ -0,0 +1,13 @@
+﻿using System.DirectoryServices.AccountManagement;
+
+namespace ActiveDirectoryManager;
+
+public class ActiveDirectoryManager
+{
+    private PrincipalContext _context;
+
+    public PrincipalContext GetUnderlyingContext()
+    {
+        return _context;
+    } 
+}
