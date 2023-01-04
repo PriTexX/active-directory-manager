df7889ce3f9865fded7c3f7b0ea38bb65c6db6ad
Lev <leomyts@gmail.com>
2023-01-04 18:58:03 +0300

    переработал класс DirectorySearcherFactory

--- ActiveDirectoryManager/SearchEngine/DirectorySearcherBuilder.cs
+++ ActiveDirectoryManager/SearchEngine/DirectorySearcherBuilder.cs
@@ -1,57 +1,34 @@
 ﻿using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
 using ActiveDirectoryManager.ActiveDirectoryItem;
+using ActiveDirectoryManager.ItemFactory;
 
 namespace ActiveDirectoryManager.SearchEngine;
 
-internal class DirectorySearcherFactory
+internal class DirectorySearcherBuilder
 {
     private PrincipalContext _principalContext;
     private string _filter = "";
     private string[]? _propertiesToLoad;
     private DomainItemType _domainItemType = DomainItemType.User;
-    
-    public static DirectorySearcherFactory CreateFactory(PrincipalContext context)
-    {
-        var factory = new DirectorySearcherFactory
-        {
-            _principalContext = context
-        };
-        return factory;
-    }
 
-    public DirectorySearcherFactory SetDomainItemType(DomainItemType domainItemType)
+    public DirectorySearcher CreateInstance(PrincipalContext context, DomainItemType domainItemType, QueryFilter queryFilter, PropertyLoader propertyLoader)
     {
+        _principalContext = context;
         _domainItemType = domainItemType;
-        return this;
-    }
-
-    public DirectorySearcherFactory SetFilter(QueryFilter queryFilter)
-    {
         _filter = FilterBuilder.BuildSearchFilter(queryFilter, _domainItemType);
-        return this;
-    }
-
-    public DirectorySearcherFactory SetPropertiesToLoad(PropertyLoader propertyLoader)
-    {
         _propertiesToLoad = PropertiesToLoadResolver.Resolve(propertyLoader);
-        return this;
-    }
-
-    public DirectorySearcher Build()
-    {
-        _propertiesToLoad ??= PropertiesToLoadResolver.GetStandardPropertiesToLoad();
+            
             
         var directorySearcher = (DirectorySearcher)GetPrincipalSearcher().GetUnderlyingSearcher();
         
         directorySearcher.Filter = _filter;
-        
         directorySearcher.PropertiesToLoad.Clear();
         directorySearcher.PropertiesToLoad.AddRange(_propertiesToLoad);
-
+        
         return directorySearcher;
     }
-    
+
     private PrincipalSearcher GetPrincipalSearcher()
     {
         return new PrincipalSearcher(new GroupPrincipal(_principalContext));
