5122d4b3384ac049d7decfe3e160457d1619897a
pritexx <vladon_2004@mail.ru>
2023-01-04 15:17:31 +0300

    added PropertyLoader class and PropertiesToLoadResolver

---
+++ ActiveDirectoryManager/SearchEngine/PropertiesToLoadResolver.cs
@@ -0,0 +1,21 @@
+﻿namespace ActiveDirectoryManager.SearchEngine;
+
+internal static class PropertiesToLoadResolver
+{
+    private static string[] _propertiesToLoad = { "adspath", "distinguishedname", "name" };
+    
+    public static void SetStandardPropertiesToLoad(PropertyLoader propertyLoader)
+    {
+        _propertiesToLoad = Resolve(propertyLoader);
+    }
+
+    public static string[] Resolve(PropertyLoader? propertyLoader)
+    {
+        return propertyLoader is null ? _propertiesToLoad : (from pair in propertyLoader.PropertyCollection where pair.Value select pair.Key).ToArray();
+    }
+
+    public static string[] GetStandardPropertiesToLoad()
+    {
+        return _propertiesToLoad;
+    }
+}
