905d7281fee438ff9f95c581eb6a2f9b9467ca96
pritexx <vladon_2004@mail.ru>
2023-01-04 15:18:15 +0300

    changed IDomainSearcher interface

--- ActiveDirectoryManager/SearchEngine/IDomainSearcher.cs
+++ ActiveDirectoryManager/SearchEngine/IDomainSearcher.cs
@@ -4,29 +4,29 @@ namespace ActiveDirectoryManager.SearchEngine;
 
 public interface IDomainSearcher
 {
-    public DomainItem? FindOne(QueryFilter queryFilter, DomainItemType type = DomainItemType.User, string[]? propertiesToLoad = null);
+    public DomainItem? FindOne(QueryFilter queryFilter, DomainItemType type = DomainItemType.User, PropertyLoader? propertiesToLoad = null);
     public IEnumerable<DomainItem?> FindAll(QueryFilter queryFilter, DomainItemType type = DomainItemType.User,
-        string[]? propertiesToLoad = null);
+        PropertyLoader? propertiesToLoad = null);
     // public IEnumerable<DomainItem?> FindAllByNames(IEnumerable<string> names, DomainItemType type = DomainItemType.User,  // TODO добавить как extension метод
-    //     string[]? propertiesToLoad = null, string? path = null, int maxQueryLength = 50);
+    //     PropertyLoader? propertiesToLoad = null, int maxQueryLength = 50);
     public IEnumerable<DomainItem?> FindAllByPropertyName(IEnumerable<string> properties, string propertyName, DomainItemType type = DomainItemType.User, 
-        string[]? propertiesToLoad = null, string? path = null, int maxQueryLength = 50);
+        PropertyLoader? propertiesToLoad = null, int maxQueryLength = 50);
     public IEnumerable<DomainItem?> FindAllByQueryFilters(IEnumerable<QueryFilter> filters, DomainItemType type = DomainItemType.User, 
-        string[]? propertiesToLoad = null, string? path = null, int maxQueryLength = 50);
-    public IEnumerable<GroupItem?> GetItemGroups(DomainItem item, string[]? propertiesToLoad = null);
-    public IEnumerable<UserItem?> GetGroupUsers(GroupItem group, string[]? propertiesToLoad = null);
+        PropertyLoader? propertiesToLoad = null, int maxQueryLength = 50);
+    public IEnumerable<GroupItem?> FindItemGroups(DomainItem item, PropertyLoader? propertiesToLoad = null);
+    public IEnumerable<UserItem?> FindGroupUsers(GroupItem group, PropertyLoader? propertiesToLoad = null);
 
     
     public Task<DomainItem?> FindOneAsync(QueryFilter queryFilter, DomainItemType type = DomainItemType.User,
-        string[]? propertiesToLoad = null);
+        PropertyLoader? propertiesToLoad = null);
     public IAsyncEnumerable<DomainItem?> FindAllAsync(QueryFilter queryFilter, DomainItemType type = DomainItemType.User,
-        string[]? propertiesToLoad = null);
+        PropertyLoader? propertiesToLoad = null);
     // public IAsyncEnumerable<DomainItem?> FindAllByNamesAsync(IEnumerable<string> names, DomainItemType type = DomainItemType.User, // TODO добавить как extension метод
-    //     string[]? propertiesToLoad = null, string? path = null, int maxQueryLength = 50);
+    //     PropertyLoader? propertiesToLoad = null, int maxQueryLength = 50);
     public IAsyncEnumerable<DomainItem?> FindAllByPropertyNameAsync(IEnumerable<string> properties, string propertyName, DomainItemType type = DomainItemType.User, 
-        string[]? propertiesToLoad = null, string? path = null, int maxQueryLength = 50);
+        PropertyLoader? propertiesToLoad = null, int maxQueryLength = 50);
     public IAsyncEnumerable<DomainItem?> FindAllByQueryFiltersAsync(IEnumerable<QueryFilter> filters, DomainItemType type = DomainItemType.User, 
-        string[]? propertiesToLoad = null, string? path = null, int maxQueryLength = 50);
-    public IAsyncEnumerable<GroupItem?> GetItemGroupsAsync(DomainItem item, string[]? propertiesToLoad = null);
-    public IAsyncEnumerable<UserItem?> GetGroupUsersAsync(GroupItem group, string[]? propertiesToLoad = null);
+        PropertyLoader? propertiesToLoad = null, int maxQueryLength = 50);
+    public IAsyncEnumerable<GroupItem?> FindItemGroupsAsync(DomainItem item, PropertyLoader? propertiesToLoad = null); 
+    public IAsyncEnumerable<UserItem?> FindGroupUsersAsync(GroupItem group, PropertyLoader? propertiesToLoad = null);
 }
