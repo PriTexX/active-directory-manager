8db97f0c8d90b8616f82a1463962cd69b822ce1c
pritexx <vladon_2004@mail.ru>
2023-01-05 20:06:47 +0300

    added SearchQuery that combines QueryFilter and PropertyLoader inside itself

---
+++ ActiveDirectoryManager/Core/Search/SearchQuery.cs
@@ -0,0 +1,36 @@
+﻿namespace ActiveDirectoryManager.Core.Search;
+
+public class SearchQuery
+{
+    private readonly PropertyLoader _propertyLoader = PropertyLoader.Empty();
+    private readonly QueryFilter _queryFilter = QueryFilter.Empty();
+
+    public static SearchQuery Create()
+    {
+        return new SearchQuery();
+    }
+
+    public SearchQuery ConfigurePropertiesToLoad(Func<PropertyLoaderConfigurator, PropertyLoaderConfigurator> propertyLoaderConfigurator)
+    {
+        var configurator = PropertyLoaderConfigurator.Create(_propertyLoader);
+        propertyLoaderConfigurator(configurator);
+        return this;
+    }
+
+    public SearchQuery ConfigureQueryFilter(Func<QueryFilterConfigurator, QueryFilterConfigurator> queryFilterConfigurator)
+    {
+        var configurator = QueryFilterConfigurator.Create(_queryFilter);
+        queryFilterConfigurator(configurator);
+        return this;
+    }
+
+    public string GetStringFilter()
+    {
+        return _queryFilter.ToStringFilter();
+    }
+
+    public string[] GetPropertiesToLoad()
+    {
+        return  PropertiesToLoadResolver.Resolve(_propertyLoader);
+    }
+}
