d3db7cb83044ae60932c935bececeecd626cf8af
pritexx <vladon_2004@mail.ru>
2023-01-17 14:53:52 +0300

    added contracts for IActiveDirectoryManager

--- ActiveDirectoryManager/Application/Common/IActiveDirectoryManager.cs
+++ ActiveDirectoryManager/Application/Common/IActiveDirectoryManager.cs
@@ -1,6 +1,29 @@
-﻿namespace ActiveDirectoryManager.Application.Common;
+﻿using ActiveDirectoryManager.Core.Entities;
+
+namespace ActiveDirectoryManager.Application.Common;
 
 public interface IActiveDirectoryManager
 {
+    public void AddToGroup(DomainItem item, GroupItem groupItem);
+    public Task AddToGroupAsync(DomainItem item, GroupItem groupItem);
+
+    public void RemoveFromGroup(DomainItem item, GroupItem groupItem);
+    public Task RemoveFromGroupAsync(DomainItem item, GroupItem groupItem);
+
+    public void Rename(DomainItem item);
+    public Task RenameAsync(DomainItem item);
+
+    public void MoveTo(DomainItem item, ContainerItem containerItem);
+    public Task MoveToAsync(DomainItem item, ContainerItem containerItem);
     
+    public void CopyTo(DomainItem item, ContainerItem containerItem);
+    public Task CopyToAsync(DomainItem item, ContainerItem containerItem);
+
+    public UserItem CreateUser(ContainerItem directory, string name, string userPassword);
+    public ContainerItem CreateContainer(ContainerItem directory, string name);
+    public GroupItem CreateGroup(ContainerItem directory, string name);
+    
+    public Task<UserItem> CreateUserAsync(ContainerItem directory, string name, string userPassword);
+    public Task<ContainerItem> CreateContainerAsync(ContainerItem directory, string name);
+    public Task<GroupItem> CreateGroupAsync(ContainerItem directory, string name);
 }
--- ActiveDirectoryManager/Infrastructure/Common/ActiveDirectoryManager.cs
+++ ActiveDirectoryManager/Infrastructure/Common/ActiveDirectoryManager.cs
@@ -1,5 +1,6 @@
 ﻿using ActiveDirectoryManager.Application.Common;
 using ActiveDirectoryManager.Application.Factories;
+using ActiveDirectoryManager.Core.Entities;
 
 namespace ActiveDirectoryManager.Infrastructure.Common;
 
@@ -10,5 +11,85 @@ public sealed class ActiveDirectoryManager : IActiveDirectoryManager
     public ActiveDirectoryManager(IActiveDirectoryConnectionFactory connectionFactory)
     {
         _connectionFactory = connectionFactory;
+    }
+
+    public void AddToGroup(DomainItem item, GroupItem groupItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddToGroupAsync(DomainItem item, GroupItem groupItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveFromGroup(DomainItem item, GroupItem groupItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveFromGroupAsync(DomainItem item, GroupItem groupItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Rename(DomainItem item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RenameAsync(DomainItem item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MoveTo(DomainItem item, ContainerItem containerItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task MoveToAsync(DomainItem item, ContainerItem containerItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(DomainItem item, ContainerItem containerItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CopyToAsync(DomainItem item, ContainerItem containerItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public UserItem CreateUser(ContainerItem directory, string name, string userPassword)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ContainerItem CreateContainer(ContainerItem directory, string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public GroupItem CreateGroup(ContainerItem directory, string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserItem> CreateUserAsync(ContainerItem directory, string name, string userPassword)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ContainerItem> CreateContainerAsync(ContainerItem directory, string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GroupItem> CreateGroupAsync(ContainerItem directory, string name)
+    {
+        throw new NotImplementedException();
     }
 }
