76868593d3bfaea53ce62fe4625d806a0874284d
pritexx <vladon_2004@mail.ru>
2023-03-07 16:49:17 +0300

    add: dependency injection extension

---
+++ ActiveDirectoryManager/Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Versioning;
+using ActiveDirectoryManager.Application;
+using ActiveDirectoryManager.Infrastructure;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+[SupportedOSPlatform("windows")]
+public static class DependencyInjectionExtension
+{
+    public static IServiceCollection AddActiveDirectoryServices(this IServiceCollection services, 
+        string domain, string user, string password, 
+        Func<IDomainSearcherBuilder, IDomainSearcherBuilder>? domainSearcherConfigurator = null)
+    {
+        var adFactory = ActiveDirectoryManagerFactory
+            .Create()
+            .SetConnectionOptions(domain, user, password);
+
+        if (domainSearcherConfigurator is not null)
+            adFactory.ConfigureDomainSearcher(domainSearcherConfigurator);
+
+        services.AddSingleton<IActiveDirectoryManager>(d => adFactory.GetActiveDirectoryManager());
+        services.AddSingleton<IDomainSearcher>(d => adFactory.GetDomainSearcher());
+        
+        return services;
+    }
+}
